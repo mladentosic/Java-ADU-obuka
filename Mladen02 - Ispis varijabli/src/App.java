a9204e0507a474d11c07d410b15377916700eca9
mladentosic <mladen.tosic.tosa@gmail.com>
2016-12-13 08:46:00 +0100

    Test sa lapTop racunara

--- Mladen02 - Ispis varijabli/src/App.java
+++ Mladen02 - Ispis varijabli/src/App.java
@@ -3,7 +3,7 @@ public class App {
 
 	public static void main(String[] args) {
 		// Definisanje varijabli
-		int mojBroj = 25;
+		int mojBroj = 75;
 		boolean mojaIstina = true;
 		// Ispis varujabli
 		System.out.println(mojBroj);
